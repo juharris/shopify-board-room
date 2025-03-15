de87cc64e9fff3c87ea3bcbac2ff21b16cb5b612
Justin Harris <juharris@users.noreply.github.com>
2025-03-14 23:11:34 -0700

    Try to get Ollama calls working

--- board-room-app/app/routes/app.chat.tsx
+++ board-room-app/app/routes/app.chat.tsx
@@ -11,59 +11,81 @@ import { TitleBar } from "@shopify/app-bridge-react";
 import { Ollama } from 'ollama';
 import { useState } from 'react';
 
+class Message {
+  role: string;
+  content: string;
+  constructor(role: string, content: string) {
+    this.role = role;
+    this.content = content;
+  }
+}
+
 export default function ChatPage() {
   const [message, setMessage] = useState('');
-  const [responses, setResponses] = useState<string[]>([]);
+  const [messages, setMessages] = useState<Message[]>([]);
   // console.log(window.location)
 
   // TODO Set host to ngrok URL.
+  // Seems to be 403 forbidden with ngrok and with the tunnel. Should work if the origin header is removed, but we can't do that from the browser.
   const client = new Ollama({
-    // proxy: true,
-    // fetch: (url, options) => {
-    //   console.log("fetch", url, options)
-    //   options.mode = "no-cors"
-    //   return fetch(url, options)
-    // }
+    host: 'https://board-room-ollama.tunnel.shopifycloud.tech',
+    // host: 'http://localhost:11434',
+    // host: 'https://354c-173-214-130-173.ngrok-free.app',
+    fetch: (url, options) => {
+      console.log("fetch", url, options)
+      options = { ...options, mode: "no-cors" }
+      return fetch(url, options)
+    }
   });
   const sendMessage = async (model: string, text: string) => {
-    const responseIndex = responses.length;
-    setResponses(prev => [...prev, ""]);
+    const message = { role: 'user', content: text }
+    const responseIndex = messages.length + 1;
+    setMessages(prev => [...prev, new Message("user", text), new Message("assistant", "")]);
 
     // TODO Add instructions with other board member profiles and use tool calling,
     // perhaps to select the board member to speak next.
-    const message = { role: 'user', content: text }
-    const response = await client.chat({
-      model,
-      messages: [message],
-      stream: true,
-    })
+    console.debug("Sending message", message)
     try {
+      const response = await client.chat({
+        model,
+        messages: [message],
+        stream: true,
+        // stream: false,
+      })
+      console.log("response", response)
+      // setMessages(prev => {
+      //   const newResponses = [...prev];
+      //   newResponses[responseIndex].content += response.message.content;
+      //   return newResponses;
+      // });
+      //*
       for await (const chunk of response) {
-        setResponses(prev => {
-          const newResponses = [...prev];
-          newResponses[responseIndex] += chunk.message.content;
-          return newResponses;
+        console.log("chunk", chunk)
+        setMessages(prev => {
+          const newMessages = [...prev];
+          newMessages[responseIndex].content += chunk.message.content;
+          return newMessages;
         });
       }
+      //*/
     } catch (error) {
-      // FIXME We don't end up here.
-      console.error("in catch")
       console.error(error)
-      setResponses(prev => {
+      setMessages(prev => {
         const newResponses = [...prev];
-        newResponses[responseIndex] = "An error occurred while generating the response. " + error;
+        newResponses[responseIndex].content = "An error occurred while generating the response. " + error;
         return newResponses;
       });
     }
   }
 
-  const handleSubmit = async () => {
-    // TODO Add model selector based on installed models from ollama.list.
-    if (message.trim()) {
-      await sendMessage('phi3', message);
-      setMessage('');
+  const handleMessageChange = async (value: string) => {
+    // TODO Allow Shift+Enter to not send the message.
+    if (value.endsWith("\n")) {
+      setMessage("");
+      await sendMessage('llama3.2', value.trimEnd());
     }
-  };
+    setMessage(value);
+  }
 
   return (
     <Page>
@@ -72,19 +94,18 @@ export default function ChatPage() {
         <Layout.Section>
           <Card>
             <BlockStack gap="300">
-              {responses.map((response, index) => (
+              {messages.map((message, index) => (
                 <Text key={index} as="p" variant="bodyMd">
-                  {response}
+                  {message.role}: {message.content}
                 </Text>
               ))}
               <TextField
                 label="Message"
                 value={message}
-                onChange={setMessage}
+                onChange={handleMessageChange}
                 multiline={3}
                 placeholder="Type your message and press Enter to send..."
                 autoComplete="off"
-                onBlur={() => handleSubmit()}
               />
             </BlockStack>
           </Card>
