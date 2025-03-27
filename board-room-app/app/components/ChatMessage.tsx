16bfe287b9bafc21690a8e4ad4c8f867902dd625
Justin Harris <juharris@users.noreply.github.com>
2025-03-27 18:12:38 -0400

    Render markdown

--- board-room-app/app/components/ChatMessage.tsx
+++ board-room-app/app/components/ChatMessage.tsx
@@ -3,12 +3,12 @@ import {
   Text,
 } from "@shopify/polaris";
 import styles from "app/styles/chat.module.css";
+import Markdown from "markdown-to-jsx";
 
 export default function ChatMessage({ message }: { message: MeetingMessage }) {
   // TODO Style messages.
   // TODO Left align messages from the user.
   // TODO Right align other messages.
-  // TODO Render content as markdown.
   // TODO Add advanced option to show tool calls and results and do not show them by default.
   switch (message.from.id) {
     case "tool_call":
@@ -26,9 +26,15 @@ export default function ChatMessage({ message }: { message: MeetingMessage }) {
   }
 
   const content = message.content
-  return (<div>
+  return (<div className={`${message.role}-message`}>
     <Text as="p" variant="bodyMd">
-      <b title={message.from.id}>{message.from.name}</b>: {content}
+      <b title={message.from.id}>{message.from.name}</b>:
+      {message.role === "assistant" ?
+        <Markdown>
+          {content}
+        </Markdown>
+        // User Message
+        : content}
     </Text>
   </div>)
 }
